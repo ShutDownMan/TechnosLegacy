dd0771bd19c174a672035edb698e73fd156cc9f2
Jedson Gabriel <jedson_gabriel@hotmail.com>
2021-02-09 11:15:06 -0300

    AutoUpdater Setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require("electron-updater")
+const isDev = require('electron-is-dev');
+
 var path = require('path');
 var win = undefined;
 
@@ -10,7 +13,7 @@ function createWindow () {
 			nodeIntegration: true
 		}
 	});
-
+	
 	win.loadFile(path.join(__dirname, './pages/index/index.html'));
 }
 
@@ -31,3 +34,49 @@ app.on('activate', () => {
 ipcMain.on('get-aluno-profile-page', function (event, args) {
 	win.loadFile(path.join(__dirname, './pages/aluno_info/aluno_info.html'));
 });
+
+var foundUpdate = false;
+var willUpdate = 0;
+const autoUpdateSetup = () => {
+	
+	autoUpdater.on('update-available', () => {
+		foundUpdate = true;
+	});
+	
+	autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+		const dialogOpts = {
+			type: 'info',
+			buttons: ['Atualizar Depois', 'Reiniciar Aplicação'],
+			title: 'Application Update',
+			message: process.platform === 'win32' ? releaseNotes : releaseName,
+			detail: 'Uma nova versão está disponível. Reinicie a aplicação para atualizar.'
+		};
+		
+		if(willUpdate === 0) {
+			dialog.showMessageBox(dialogOpts).then((returnValue) => {
+				willUpdate = returnValue.response;
+				if (willUpdate === 1) autoUpdater.quitAndInstall();
+			});
+		}
+	});
+	
+	autoUpdater.on('error', message => {
+		console.error('There was a problem updating the application');
+		console.error(message);
+	});
+	
+	autoUpdater.checkForUpdates();
+	
+	setInterval(() => {
+		if(foundUpdate === false)
+		autoUpdater.checkForUpdates();
+	}, 60000)
+}
+
+function main() {
+	if (!isDev) {
+		autoUpdateSetup();
+	}
+}
+
+main();
--- src/pages/index/js/main.js
+++ src/pages/index/js/main.js
@@ -33,7 +33,7 @@ function initializeList() {
 	column1 = document.querySelector('#table_column1');
 	column2 = document.querySelector('#table_column2');
 	currentPage = document.querySelector('#current_page')
-
+	
 	column1.innerHTML = "ID";
 	column2.innerHTML = "Nome";
 	currentPage.innerHTML = "Home";
@@ -136,8 +136,62 @@ function getFollowUpsByIDAluno(idAluno) {
 					// console.log(await getDescricao(currentValue));
 					return {column1: currentValue.DATA_CONTATO, column2: await getDescricao(currentValue)};
 				});
-
+				
 				resolve(Promise.all(result));
+				
+				db.detach();
+			});
+		});
+	}); 
+}
+
+function getNotasByIDAluno(idAluno) {
+	return new Promise(function (resolve, reject) {
+		dbPool.get(function (err, db) {
+			if(err) throw err;
+			
+			db.query(`SELECT r.DATA, r.PRESENCA' FROM PRESENCA r WHERE r.ID_ALUNO = ?`, [parseInt(idAluno)],
+			async function(err, result, e) {
+				if(err) throw err;
+				
+				result = result.map(function(currentValue, index, arr) {
+					return {column1: currentValue.DATA, column2: currentValue.PRESENCA};
+				});
+				
+				resolve(result);
+				
+				db.detach();
+			});
+		});
+	}); 
+}
+
+function getPresencasByIDAluno(idAluno) {
+	return new Promise(function (resolve, reject) {
+		dbPool.get(function (err, db) {
+			if(err) throw err;
+			
+			db.query(`SELECT r.DATA, r.PRESENCA, r.COMPUTADOR, r.LABORATORIO FROM PRESENCA r WHERE r.ID_ALUNO = ?`, [parseInt(idAluno)],
+			async function(err, result, e) {
+				if(err) throw err;
+				
+				console.log(result);
+				
+				result = result.map(function(currentValue, index, arr) {
+					let column2Str = `(${currentValue.LABORATORIO} @ ${currentValue.COMPUTADOR}) - ${currentValue.PRESENCA}`;
+					if(!currentValue.LABORATORIO) {
+						if(!currentValue.COMPUTADOR) {	
+							column2Str = `${currentValue.PRESENCA}`;
+						} else {
+							column2Str = `(${currentValue.COMPUTADOR}) ${currentValue.PRESENCA}`;
+						}
+					} else if(!currentValue.COMPUTADOR) {
+						column2Str = `(${currentValue.LABORATORIO}) ${currentValue.PRESENCA}`;
+					}
+					return {column1: currentValue.DATA, column2: column2Str};
+				});
+				
+				resolve(result);
 				
 				db.detach();
 			});
@@ -167,11 +221,14 @@ function getFollowUpsByIDAluno(idAluno) {
 
 function onSearchChanged(newText) {
 	console.log(newText);
-
+	
 	currentPage.innerHTML = "Home";
 	
 	/// if less than 3 chars, don't search
 	if(newText.length < 4) return;
+	
+	column1.innerHTML = "ID";
+	column2.innerHTML = "Nome";
 	
 	/// get results that match
 	let result = searcher.search(newText);
@@ -197,7 +254,8 @@ var CurrentAlunoInfo = undefined;
 async function onClickAluno(idAluno, nomeAluno) {
 	// console.log(idAluno);
 	// console.log(nomeAluno);
-
+	
+	/// se menu atual não for home
 	if(currentPage.innerHTML !== "Home") {
 		return;
 	}
@@ -224,12 +282,12 @@ async function onClickFollowUpsAluno() {
 	let followUps = await getFollowUpsByIDAluno(CurrentIDAluno);
 	
 	currentPage.innerHTML = 'Follow Ups';
-
+	
 	console.log("FOLLOW UPS:");
 	console.log(followUps);
 	
 	column1.innerHTML = "Data";
-	column2.innerHTML = "Descrição";	
+	column2.innerHTML = "Descrição";
 	
 	mainList.clear();
 	if(!followUps.length) {	
@@ -237,7 +295,42 @@ async function onClickFollowUpsAluno() {
 	}
 	
 	mainList.add(followUps);
-	// mainList.clear();
+}
+
+async function onClickPresencasAluno() {
+	let presencas = await getPresencasByIDAluno(CurrentIDAluno);
 	
-	// mainList.add(followUps);
+	currentPage.innerHTML = 'Presenças';
+	
+	console.log("Presenças:");
+	console.log(presencas);
+	
+	column1.innerHTML = "Data";
+	column2.innerHTML = "Comparecimento";	
+	
+	mainList.clear();
+	if(!presencas.length) {	
+		mainList.add([{column1: -1, column2: 'Sem resultados'}]);
+	}
+	
+	mainList.add(presencas);
+}
+
+async function onClickModulosAluno() {
+	let notas = await getModulosByIDAluno(CurrentIDAluno);
+	
+	currentPage.innerHTML = 'Notas';
+	
+	console.log("Notas:");
+	console.log(notas);
+	
+	column1.innerHTML = "Média";
+	column2.innerHTML = "Módulos";
+	
+	mainList.clear();
+	if(!notas.length) {	
+		mainList.add([{column1: -1, column2: 'Sem resultados'}]);
+	}
+	
+	mainList.add(notas);
 }
